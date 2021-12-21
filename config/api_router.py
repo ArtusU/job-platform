4d902036f5caf72b35eb83178dcef604af0ce3a6
Artus U <artusrock@hotmail.com>
2021-12-21 11:23:34 +0000

    JobDetailView  JobDeleteView

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import path
-from djangojobboard.jobs.api.view import JobListView, JobCreateView, JobUpdateView
+from djangojobboard.jobs.api.view import JobListView, JobCreateView, JobUpdateView, JobDeleteView, JobDetailView
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from djangojobboard.users.api.views import UserViewSet
@@ -17,7 +17,9 @@ app_name = "api"
 
 urlpatterns = [
     path("jobs/", JobListView.as_view()),
-    path("jobs/<pk>/", JobUpdateView.as_view()),
+    path("jobs/<pk>/", JobDetailView.as_view()),
+    path("jobs/<pk>/update/", JobUpdateView.as_view()),
+    path("jobs/<pk>/delete/", JobDeleteView.as_view()),
     path("create-job/", JobCreateView.as_view()),
 ]
 urlpatterns += router.urls
--- djangojobboard/jobs/api/view.py
+++ djangojobboard/jobs/api/view.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 from djangojobboard.jobs.models import Job
 from .serializers import JobSerializer
 
@@ -16,9 +16,20 @@ class JobCreateView(CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         
-        
+class JobDetailView(RetrieveAPIView):
+    serializer_class = JobSerializer
+
+    def get_queryset(self):
+        return Job.objects.all()
+    
+     
 class JobUpdateView(UpdateAPIView):
     serializer_class = JobSerializer
 
     def get_queryset(self):
         return Job.objects.filter(available=True)
+    
+
+class JobDeleteView(DestroyAPIView):
+    def get_queryset(self):
+        return Job.objects.all()
