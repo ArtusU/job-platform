a962d9be4553f47f378a9f53ee68326d488af3d2
Artus U <artusrock@hotmail.com>
2021-12-22 09:24:04 +0000

    react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+function JobList() {
 
   const [jobs, setJobs] = useState(null)
 
@@ -29,4 +35,39 @@ function App() {
   );
 }
 
-export default App;
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About/>} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<JobList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
