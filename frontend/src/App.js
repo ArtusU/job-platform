41cd4c6030d8c0e03c584727990793dca82b4b69
Artus U <artusrock@hotmail.com>
2021-12-24 11:03:34 +0000

    PrivateRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,72 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { AuthContextProvider } from './contexts/AuthContext'
-import { JobList } from './components/JobList'
-import { JobDetail } from './components/JobDetail'
-import { JobCreate } from './components/JobCreate'
-import { Login } from './components/Login'
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext, AuthContextProvider } from "./contexts/AuthContext";
+import { JobList } from "./components/JobList";
+import { JobDetail } from "./components/JobDetail";
+import { JobCreate } from "./components/JobCreate";
+import { Login } from "./components/Login";
 import { Navbar } from "./components/Navbar";
 import { JobUpdate } from "./components/JobUpdate";
 import { JobDelete } from "./components/JobDelete";
+
+function PrivateRoute({ children }) {
+  const { user } = useContext(AuthContext);
+  return user ? children : <Navigate replace to="/login" />;
+}
 
 export default function App() {
   return (
     <Router>
       <AuthContextProvider>
         <div>
-          <Navbar />          
+          <Navbar />
 
           {/* A <Routes> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
-          <Routes>
-            <Route path="/about" element={<About/>} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/jobs/:id" element={<JobDetail />} />
-            <Route path="/jobs/:id/update" element={<JobUpdate />} exact/>
-            <Route path="/jobs/:id/delete" element={<JobDelete />} exact/>
-            <Route path="/create-job" element={<JobCreate />} exact />
-            <Route path="/login" element={<Login />} exact />
-            <Route path="/" element={<JobList />} exact />
-          </Routes>
+          <div className="max-w-4xl mx-auto py-5 px-4">
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/users" element={<Users />} />
+              <Route
+                path="/jobs/:id"
+                element={
+                  <PrivateRoute>
+                    <JobDetail />
+                  </PrivateRoute>
+                }
+                exact
+              />
+              <Route
+                path="/jobs/:id/update"
+                element={
+                  <PrivateRoute>
+                    <JobUpdate />
+                  </PrivateRoute>
+                }
+                exact
+              />
+              <Route
+                path="/jobs/:id/delete"
+                element={
+                  <PrivateRoute>
+                    <JobDelete />
+                  </PrivateRoute>
+                }
+                exact
+              />
+              <Route
+                path="/create-job"
+                element={
+                  <PrivateRoute>
+                    <JobCreate />
+                  </PrivateRoute>
+                }
+                exact
+              />
+              <Route path="/login" element={<Login />} exact />
+              <Route path="/" element={<JobList />} exact />
+            </Routes>
+          </div>
         </div>
       </AuthContextProvider>
     </Router>
